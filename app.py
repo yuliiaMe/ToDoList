6dd46aaf0a5753e787a0334da519f2be4d0feb6d
Yuliasha <yuliia.kryvosheia@gmail.com>
2026-07-20 23:45:24 +0300

    fix: repair railway url and open cors

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 local_db = 'mysql+mysqlconnector://root:qwerPoiu1010+@localhost/todo_db'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', local_db)
